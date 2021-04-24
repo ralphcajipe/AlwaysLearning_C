f3e3239c9884caa09969d93f868d5e69834f5b01
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2021-04-24 22:14:51 +0800

    The first C program

---
+++ the_first_c_program.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // include information about the standard input/output library
+
+main() // define a function named main that receives no argument values
+{
+	printf("hello, world!\n"); 
+	/**
+	* main calls library function printf 
+	* to print this sequence of characters;
+	* \n represents the newline character
+	*/
+}
