4e3f4d82eb2b0a03a86fea10f8585a9dddf7afb9
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2021-04-26 22:42:08 +0800

    Update 8_copy_input_to_output_first_version.c

--- Chapter_1_Introduction/8_copy_input_to_output_first_version.c
+++ Chapter_1_Introduction/8_copy_input_to_output_first_version.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+/* pseudocode:
+ *
+ * read a character
+ * while (character is not end-of-file indicator)
+ *    output the character just read
+ *    read a character
+ */
+main()
+{
+	int c;
+	
+	c = getchar();
+	while(c != EOF){
+		putchar(c);
+		c = getchar();
+	}
+}
