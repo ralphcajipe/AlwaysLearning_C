9220b8b14672254877a4c8ae505057f947da1350
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2021-04-26 22:26:54 +0800

    Update 7_fahrenheit_to_celsius_constants.c

--- Chapter_1_Introduction/7_fahrenheit_to_celsius_constants.c
+++ Chapter_1_Introduction/7_fahrenheit_to_celsius_constants.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+#define LOWER 0     /* lower limit of table */
+#define UPPER 300   /* upper limit of table */
+#define STEP 20     /* step size */
+
+/* print Fahrenheit to Celsius table using symbolic constants */
+
+main()
+{
+  int fahrenheit;
+  
+  printf("Fahrenheit\tCelsius\n");
+  for(fahrenheit = LOWER; fahrenheit <= UPPER; fahrenheit = fahrenheit + STEP)
+  	printf("%5d %15.1f\n", fahrenheit, (5.0/9.0) * (fahrenheit-32) );
+}
